65e8e92b439baaa163f308f060c5ba024bae9c1b
Hack-sudo-hub <zakariabiodun@gmail.com>
2025-08-02 15:39:20 +0100

    adding backend server

---
+++ controllers/usersController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const db = require('../config/db');
+const { response } = require('express');
+
+
+exports.registerUser = async (req, res) => {
+    const {
+        first_name,
+        last_name,
+        email,
+        password,
+        phone,
+        gender,
+        address,
+
+    } = req.body;
+
+    try {
+        //checking if user exist
+        const [rows] = await db.execute('SELECT * FROM users WHERE email = ?', [email,]);
+        if  (rows.length > 0) {
+            return response.status(400).json({message: 'User Already exists!'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const sql =
+        'INSERT INTO users (first_name, last_name, email, password_hash, phone, gender, address) VALUES (?, ?, ?, ?, ?, ?, ?)';
+        db.execute(sql, [first_name,
+        last_name,
+        email,
+        hashedPassword,
+        phone,
+        gender,
+        address,]
+
+        );
+
+        res.status(201).json({message: 'User registered successfully!'}); 
+    
+     } catch (error) {
+            res.status(500).json({message: 'An error occurred!', error});
+        }
+
+};
---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session") (session);
+const bodyParser = require("body-parser");
+const routes = require("./routes/auth");
+const db = require("./config/db");
+
+// initialize express app
+const app = express();
+
+// Configure the session store
+const sessionStore = new MySQLStore({}, db);
+
+// Set up session middleware 
+app.use(
+    session({
+        key: "user_id",
+        secret: process.env.SESSION_SECRET, 
+        store: sessionStore, //use the MySQL store for session
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: false,
+            maxAge: 1000 * 60 * 24, // 1 day
+        }
+    })
+);
+
+// Middleware
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "/")));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/auth", routes);
+
+
+// Start the server
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+    console.log(`Server runinng on http://localhost:${PORT}`)
+);
